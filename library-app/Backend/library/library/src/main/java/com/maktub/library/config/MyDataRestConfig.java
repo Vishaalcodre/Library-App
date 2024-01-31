111ea70409c0311902cd6341d11a7fab18e2b2e3
Vishaalcodre <vishaa369@gmail.com>
2024-01-31 16:05:47 +0530

    Reviews

--- library-app/Backend/library/library/src/main/java/com/maktub/library/config/MyDataRestConfig.java
+++ library-app/Backend/library/library/src/main/java/com/maktub/library/config/MyDataRestConfig.java
@@ -2,6 +2,7 @@ package com.maktub.library.config;
 
 
 import com.maktub.library.entity.Book;
+import com.maktub.library.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -17,8 +18,10 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         HttpMethod[] theUnsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.PATCH};
 
         config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
 
 //        Configure CORS Mapping
         cors.addMapping(config.getBasePath() + "/**")
--- library-app/Frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-app/Frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -3,12 +3,20 @@ import BookModel from "../../models/BookModel"
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "../SearchBooksPage/CheckoutAndReviewBox";
+import { error } from "console";
+import { LatestReviews } from "./LatestReviews";
+import ReviewModel from "../../models/ReviewModel";
 
 export const BookCheckoutPage = () => {
 
     const [book, setBook] = useState<BookModel>();
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
+
+    //Reviews
+    const [review, setReview] = useState<ReviewModel[]>([]);
+    const [totalStars, setTotalStars] = useState(0);
+    const [isLoadingReview, setIsLoadingReview] = useState(true);
 
     const bookId = (window.location.pathname).split('/')[2];
 
@@ -47,7 +55,54 @@ export const BookCheckoutPage = () => {
         })
     }, [])
 
-    if (isLoading) {
+    useEffect(() => {
+        const fetchBookReviews = async () => {
+            const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+
+            const responseReviews = await fetch(reviewUrl);
+
+            if(!responseReviews.ok){
+                throw new Error("Something went wrong!");
+            }
+
+            const responseJsonReviews = await responseReviews.json();
+            
+            const responseData = responseJsonReviews._embedded.reviews;
+
+            const loadedReviews:ReviewModel[] = [];
+
+            let weightedStarReviews: number = 0;
+
+            for(const key in responseData){
+                loadedReviews.push({
+                    id: responseData[key].id,
+                    userEmail: responseData[key].userEmail,
+                    date: responseData[key].date,
+                    rating: responseData[key].rating,
+                    book_id: responseData[key].bookId,
+                    reviewDescription: responseData[key].reviewDescription,
+                });
+            
+                weightedStarReviews = weightedStarReviews + responseData[key].rating;
+            }
+
+            if(loadedReviews){
+                const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2 ) / 2).toFixed(1);
+
+                setTotalStars(Number(round));
+            }
+
+            setReview(loadedReviews);
+            setIsLoadingReview(false);
+        };
+
+        fetchBookReviews().catch((error:any) => {
+            setIsLoadingReview(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading || isLoadingReview) {
         return (
             <SpinnerLoading />
         );
@@ -60,6 +115,7 @@ export const BookCheckoutPage = () => {
             </div>
         );
     }
+
 
     return (
         <div>
@@ -77,12 +133,13 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className="text-primary">{book?.author}</h5>
                             <p className="lead">{book?.description}</p>
-                            <StarsReview rating={4} size={32} />
+                            <StarsReview rating={totalStars} size={32} />
                         </div>
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false} />
                 </div>
                 <hr />
+                <LatestReviews reviews={review} bookId={book?.id} mobile={false} />
             </div>
 
             {/* Mobile */}
@@ -99,11 +156,12 @@ export const BookCheckoutPage = () => {
                         <h2>{book?.title}</h2>
                         <h5 className="text-primary">{book?.author}</h5>
                         <p className="lead">{book?.description}</p>
-                        <StarsReview rating={5} size={32} />
+                        <StarsReview rating={totalStars} size={32} />
                     </div>
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true} />
                 <hr />
+                <LatestReviews reviews={review} bookId={book?.id} mobile={true} />
             </div>
         </div>
     )
