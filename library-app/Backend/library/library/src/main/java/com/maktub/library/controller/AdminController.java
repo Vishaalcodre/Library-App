062e5b4bdc499ecdf94827347b6f6451971faccb
Vishaalcodre <vishaa369@gmail.com>
2024-02-08 00:10:21 +0530

    Admin new and change book Functionality

---
+++ library-app/Backend/library/library/src/main/java/com/maktub/library/controller/AdminController.java
@@ -0,0 +1,59 @@
+package com.maktub.library.controller;
+
+import com.maktub.library.requestmodels.AddBookRequest;
+import com.maktub.library.service.AdminService;
+import com.maktub.library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.decreaseBookQuantity(bookId);
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+    @DeleteMapping("/secure/delete/book")
+    public void deleteBook(@RequestHeader(value="Authorization") String token,
+                           @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.deleteBook(bookId);
+    }
+}
