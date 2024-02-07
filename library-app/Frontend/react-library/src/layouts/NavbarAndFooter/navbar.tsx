ea357f6486f73a35971c95f3222bdb2db4267071
Vishaalcodre <vishaa369@gmail.com>
2024-02-08 00:10:59 +0530

    Admin Navbar

--- library-app/Frontend/react-library/src/layouts/NavbarAndFooter/navbar.tsx
+++ library-app/Frontend/react-library/src/layouts/NavbarAndFooter/navbar.tsx
@@ -46,6 +46,14 @@ export const Navbar = () => {
               </li>
               }
 
+              {authState.isAuthenticated && authState.accessToken?.claims?.userType==="admin" &&
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/admin">
+                    Admin
+                  </NavLink>
+                </li>
+              }
+
           </ul>
           <ul className='navbar-nav ms-auto'>
 
