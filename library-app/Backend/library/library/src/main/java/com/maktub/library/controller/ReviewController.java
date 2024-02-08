3f870967dd864a8860d6485dda295108b04f5a53
Vishaalcodre <vishaa369@gmail.com>
2024-02-08 10:58:50 +0530

    HTTPS frontend(React)

--- library-app/Backend/library/library/src/main/java/com/maktub/library/controller/ReviewController.java
+++ library-app/Backend/library/library/src/main/java/com/maktub/library/controller/ReviewController.java
@@ -5,7 +5,7 @@ import com.maktub.library.service.ReviewService;
 import com.maktub.library.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://localhost:3000")
 @RestController
 @RequestMapping("api/reviews")
 public class ReviewController {
